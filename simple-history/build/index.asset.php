5055612c7ab30e5f13979c47f5524c2441f7dd5b
Sean Murphy <sean@iamseanmurphy.com>
2026-04-12 10:24:20 +0000

    5.26.0

--- simple-history/build/index.asset.php
+++ simple-history/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'b13945ad7b3f46a02271');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '6a670281be230c22107f');
